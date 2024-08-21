38c18862f60aad0e44310c9ec41b2e721017e2b4
mwlasiuk <testmailsmtp12345@gmail.com>
2024-08-21 17:45:27 +0200

    Remove CSVs

--- make-pointcloud.py
+++ make-pointcloud.py
@@ -9,9 +9,10 @@ landmarks = dict(data['landmarks'].items())
 keyframes = dict(data['keyframes'].items())
 
 with open('poses.csv', 'w') as poses_file:
-    # poses_file.write('# id rot_cw_x rot_cw_y rot_cw_z rot_cw_w trans_cw_x trans_cw_y trans_cw_z trans_wc_x trans_wc_y trans_wc_z\n')
+    poses_file.write('# id, rot_cw_x, rot_cw_y, rot_cw_z, rot_cw_w, trans_cw_x, trans_cw_y, trans_cw_z, trans_wc_x, trans_wc_y, trans_wc_z\n')
 
     for keyframe_id, keyframe in keyframes.items():
+
         rot_cw = keyframe['rot_cw']
         trans_cw = keyframe['trans_cw']
 
@@ -28,7 +29,7 @@ with open('poses.csv', 'w') as poses_file:
         t_y = M_inv[1, 3]
         t_z = M_inv[2, 3]
 
-        poses_file.write(f'{keyframe_id} {rot_cw[0]} {rot_cw[1]} {rot_cw[2]} {rot_cw[3]} {trans_cw[0]} {trans_cw[1]} {trans_cw[2]} {t_x} {t_y} {t_z}\n');
+        poses_file.write(f'{keyframe_id}, {rot_cw[0]}, {rot_cw[1]}, {rot_cw[2]}, {rot_cw[3]}, {trans_cw[0]}, {trans_cw[1]}, {trans_cw[2]}, {t_x}, {t_y}, {t_z}\n');
 
         with open(f'landmarks-for-keyframe-{keyframe_id}.csv', 'w') as keyframe_landmarks_file:
             for landmark_id in keyframe['lm_ids']:
